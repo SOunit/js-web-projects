7652853108d320422820af9000f516497da586d0
SOunit <workbookwork7@gmail.com>
2022-03-19 15:27:24 -0700

    add sample

--- 01_original/03_quote-generator/script.js
+++ 01_original/03_quote-generator/script.js
@@ -1,9 +1,9 @@
-const quoteContainer = document.getElementById('quote-container');
-const quoteText = document.getElementById('quote');
-const authorText = document.getElementById('author');
-const twitterBtn = document.getElementById('twitter');
-const newQuoteBtn = document.getElementById('new-quote');
-const loader = document.getElementById('loader');
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 // Loading Spinner Shown
 function loading() {
@@ -23,22 +23,23 @@ function complete() {
 async function getQuote() {
   loading();
   // We need to use a Proxy URL to make our API call in order to avoid a CORS error
-  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-  const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const apiUrl =
+    "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
   try {
     const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
     // Check if Author field is blank and replace it with 'Unknown'
-    if (data.quoteAuthor === '') {
-      authorText.innerText = 'Unknown';
+    if (data.quoteAuthor === "") {
+      authorText.innerText = "Unknown";
     } else {
       authorText.innerText = data.quoteAuthor;
     }
     // Dynamically reduce font size for long quotes
     if (data.quoteText.length > 120) {
-      quoteText.classList.add('long-quote');
+      quoteText.classList.add("long-quote");
     } else {
-      quoteText.classList.remove('long-quote');
+      quoteText.classList.remove("long-quote");
     }
     quoteText.innerText = data.quoteText;
     // Stop Loading, Show Quote
@@ -53,12 +54,12 @@ function tweetQuote() {
   const quote = quoteText.innerText;
   const author = authorText.innerText;
   const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
-  window.open(twitterUrl, '_blank');
+  window.open(twitterUrl, "_blank");
 }
 
 // Event Listeners
-newQuoteBtn.addEventListener('click', getQuote);
-twitterBtn.addEventListener('click', tweetQuote);
+newQuoteBtn.addEventListener("click", getQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 // On Load
 getQuote();
